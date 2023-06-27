0f1d14bf7727ca57ebdc269d8c6351b64a452926
sunl <1255193087@qq.com>
2023-06-27 11:52:42 +0800

    001基础

--- 001/数值.py
+++ 001/数值.py
@@ -3,3 +3,30 @@
 # @Time     : 2023/6/26 17:12
 # @Author   : SLNotFound
 # @File     : 数值.py
+
+# 定义变量a
+a = 56
+print(a)
+
+b = 9999999999999999999
+print(b)
+print(type(b))
+
+hex_value = 0x13
+print("hex_value的值为：", hex_value)
+
+bin_value = 0b111
+print("bin_value的值为：", bin_value)
+
+oct_value = 0o54
+print("oct_value的值为：", oct_value)
+
+# 浮点型
+f1 = 5.334234
+print("f1的值是：", f1)
+
+f2 = 5.12e2
+print("f2的值为：", f2)
+
+f3 = 5e3
+print("f3的值为：", f3)
