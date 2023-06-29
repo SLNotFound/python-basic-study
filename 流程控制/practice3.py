24c617b8911ac6d12b7481afb08fceb123ee8c70
sunl <1255193087@qq.com>
2023-06-29 16:08:54 +0800

    for循环例子

---
+++ 流程控制/practice3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time     : 2023/6/29 16:06
+# @Author   : SLNotFound
+# @File     : practice3.py
+
+"""
+计算1~10的整数之和
+"""
+result =0
+for i in range(1, 11):
+    result += i
+print(result)
