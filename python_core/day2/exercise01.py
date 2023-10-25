a920c71cf989e5210be113767ab4cbaba0ea9c3f
sunl <1255193087@qq.com>
2023-10-25 09:49:49 +0800

    练习

---
+++ python_core/day2/exercise01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time     : 2023/10/17 21:32
+# @Author   : SLNotFound
+# @File     : exercise01.py
+
+"""
+    汇率转换器
+"""
+usd = float(input("请输入美元："))
+cny = usd * 6.405
+print(str(usd) + "美元=" + str(cny) + "人民币")
---
+++ python_core/day2/exercise03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time     : 2023/10/17 21:53
+# @Author   : SLNotFound
+# @File     : exercise03.py
+"""
+效果：
+请输入I kiss you的主语:I
+请输入I kiss you的谓语:kiss
+请输入I kiss you的宾语:you
+您输入的主语是:I,谓语是:kiss,宾语是:you
+"""
+str1 = input("请输入I kiss you的主语:")
+str2 = input("请输入I kiss you的谓语:")
+str3 = input("请输入I kiss you的宾语:")
+print("您输入的主语是:" + str1 + ",谓语是:" + str2 + ",宾语是:" + str3)
